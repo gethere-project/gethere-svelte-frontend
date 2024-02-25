6f95d82466cb9be5e7c9d90fc22e30419c0b0180
Krystian Wiecheć <krystian.wiechec.pub@krystejj.me>
2024-02-25 21:58:46 +0100

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+  kit: {
+    adapter: adapter(),
+  },
+};
+
+export default config;
